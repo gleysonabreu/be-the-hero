3da9a2aeb321c77b097e958654881c38244066d2
Gleyson Abreu <gleyson_datu@hotmail.com>
2020-04-02 15:13:19 -0300

    Fix scrolling to top after click on close modal

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiX } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import loadingImage from '../../assets/loading.svg'
 import './styles.css';
 
@@ -9,7 +10,7 @@ function Modal({ message, display, loading, close }){
     <div className={`${ display ? 'modalShow' : 'modalHide' } modal`}>
       <div className="content">
        <div className="close">
-         <a href="#" onClick={() => close()}><FiX size={20} color="#E02041"/></a>
+         <Link to='#' onClick={() => close()}><FiX size={20} color="#E02041"/></Link>
        </div>
        <div className="wrapper">
           <img className="loading" src={loadingImage} alt="Loading" style={{ display: loading ? 'block' : 'none' }}/>
